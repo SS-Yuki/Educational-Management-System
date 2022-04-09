5ae61d61c8d1a2fbd2ac7e3819fb3f80a36f14d8
wangjunfei <471612627@qq.com>
2022-04-09 16:11:53 +0800

    测试element

--- lab3_front/src/main.js
+++ lab3_front/src/main.js
@@ -5,5 +5,6 @@ import store from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+import '@/assets/css/global.css'
 
 createApp(App).use(store).use(ElementPlus).use(router).mount('#app')
--- lab3_front/src/views/Student.vue
+++ lab3_front/src/views/Student.vue
@@ -1,5 +1,32 @@
 <template>
 <div>Student</div>
+  <div>
+    <el-form
+        ref="formRef"
+        :model="numberValidateForm"
+        label-width="100px"
+        class="demo-ruleForm"
+    >
+      <el-form-item
+          label="age"
+          prop="age"
+          :rules="[
+        { required: true, message: 'age is required' },
+        { type: 'number', message: 'age must be a number' },
+      ]"
+      >
+        <el-input
+            v-model.number="numberValidateForm.age"
+            type="text"
+            autocomplete="off"
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm(formRef)">Submit</el-button>
+        <el-button @click="resetForm(formRef)">Reset</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>

118dcdf7f3dc48e219ec1e96cd15b0af26f1a1be
YuKang <2833721527@qq.com>
2022-04-10 22:23:28 +0800

    test

--- lab3_front/src/views/Register.vue
+++ lab3_front/src/views/Register.vue
@@ -2,32 +2,32 @@
   <div>
     <div>Register</div>
     <div class="register">
-      <el-form :model="register_data" label-width="120px" rules="rules">
-        <el-form-item label="身份">
+      <el-form :model="register_data" label-width="120px" :rules="rules">
+        <el-form-item label="身份" prop="role">
           <el-radio-group v-model="register_data.role">
             <el-radio label="学生" />
             <el-radio label="教师" />
           </el-radio-group>
         </el-form-item>
-          <el-form-item label="院系/专业">
+          <el-form-item label="院系/专业" prop="school_major">
             <el-cascader  v-model="school_major" :options="options" @change="school_major_select"/>
           </el-form-item>
-        <el-form-item label="学号/工号">
+        <el-form-item label="学号/工号" prop="number">
           <el-input v-model="register_data.number" />
         </el-form-item>
-        <el-form-item label="姓名">
+        <el-form-item label="姓名" prop="name">
           <el-input v-model="register_data.name" />
         </el-form-item>
-        <el-form-item label="身份证号">
+        <el-form-item label="身份证号" prop="idNum">
           <el-input v-model="register_data.idNum" />
         </el-form-item>
-        <el-form-item label="手机号">
+        <el-form-item label="手机号" prop="phoneNum">
           <el-input v-model="register_data.phoneNum"/>
         </el-form-item>
         <el-form-item label="电子邮箱" prop="email">
           <el-input v-model="register_data.email" />
         </el-form-item>
-        <el-form-item label="密码">
+        <el-form-item label="密码" prop="password">
           <el-input v-model="register_data.password" />
         </el-form-item>
         <el-button style="width: 100%" type="primary" @click="register">注册</el-button>
@@ -71,14 +71,15 @@ export default {
         password: ''
       },
       rules: {
-        role:[{ required: true, message: '请输入医生名称', trigger: 'blur' }],
-        number: [{ required: true, message: '请选择职称', trigger: 'change' }],
-        name: [{ required: true, message: '请选择性别', trigger: 'change' }],
-        idNum: [{ required: true, message: '请选择医院', trigger: 'change' }],
-        phoneNum: [{ required: true, message: '请选择职称', trigger: 'change' },
+        role:[{ required: true, message: '请选择', trigger: 'blur' }],
+        school_major:[{ required: true, message: '请输入医生名称', trigger: 'blur' }],
+        number: [{ required: true, message: '请选择职称', trigger: 'blur' }],
+        name: [{ required: true, message: '请选择性别', trigger: 'blur' }],
+        idNum: [{ required: true, message: '请选择医院', trigger: 'blur' }],
+        phoneNum: [{ required: true, message: '请选择职称', trigger: 'blur' },
           { pattern: /^(13[0-9]|14[1|4|5|6|7|8|9]|15[0|1|2|3|5|6|7|8|9]|17[2|3|5|6|7|8]|18[0-9]|19[1|8|9])\d{8}$/, message: '手机号格式不正确', trigger: 'blur'}],
         email: [{ required: true, message: '请选择性别', trigger: 'blur' }],
-        password: [{ required: true, message: '请选择医院', trigger: 'change' }]
+        password: [{ required: true, message: '请选择医院', trigger: 'blur' }]
       }
     }
   },

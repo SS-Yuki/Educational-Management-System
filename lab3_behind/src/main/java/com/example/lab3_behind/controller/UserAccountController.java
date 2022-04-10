28c517b9192a0f409d84031f0b044ad0dd88d9b2
zhangjiaxun <20302010032@fudan.edu.cn>
2022-04-10 09:01:24 +0800

    实现了登录的请求

--- lab3_behind/src/main/java/com/example/lab3_behind/controller/UserAccountController.java
+++ lab3_behind/src/main/java/com/example/lab3_behind/controller/UserAccountController.java
@@ -1,4 +1,53 @@
 package com.example.lab3_behind.controller;
 
+
+import com.example.lab3_behind.domain.UserAccount;
+import com.example.lab3_behind.service.UserAccountService;
+import com.example.lab3_behind.utils.JwtUtil;
+import com.example.lab3_behind.vo.JwtUserData;
+import com.example.lab3_behind.vo.LoginUserData;
+import com.example.lab3_behind.vo.Result;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/user")
 public class UserAccountController {
+    @Autowired
+    UserAccountService userAccountService;
+
+//    @RequestMapping("/login")
+//    public Result login(@RequestBody LoginUserData user){
+//        Map<String,Object> map = new HashMap<>();
+//        try {
+//            UserAccount userAccount = userAccountService.login(user);
+//            JwtUserData jwtUserData = JwtUserData.accountToJwt(userAccount);
+//            String token = JwtUtil.createToken(jwtUserData);
+//            map.put("code",200);
+//            map.put("msg","success");
+//            map.put("token",token);
+//        }catch (Exception e){
+//            return Result.fail(620,e.getMessage());
+//        }
+//        return Result.succ(map);
+//    }
+
+    @PostMapping("/login")
+    public Result login(@RequestBody LoginUserData user){
+        String token;
+        try {
+            UserAccount userAccount = userAccountService.login(user);
+            JwtUserData jwtUserData = JwtUserData.accountToJwt(userAccount);
+            token = JwtUtil.createToken(jwtUserData);
+
+        }catch (Exception e){
+            return Result.fail(620,e.getMessage());
+        }
+        return Result.succ(token);
+    }
+
 }

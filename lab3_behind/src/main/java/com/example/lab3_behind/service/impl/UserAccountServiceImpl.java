44fccc7c287adb72e8e4594b460cdf94799d5faf
zhangjiaxun <20302010032@fudan.edu.cn>
2022-04-10 21:18:03 +0800

    aa

---
+++ lab3_behind/src/main/java/com/example/lab3_behind/service/impl/UserAccountServiceImpl.java
@@ -0,0 +1,30 @@
+package com.example.lab3_behind.service.impl;
+
+import com.example.lab3_behind.domain.UserAccount;
+import com.example.lab3_behind.repository.UserAccountRepository;
+import com.example.lab3_behind.service.UserAccountService;
+import com.example.lab3_behind.domain.dto.LoginUserData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserAccountServiceImpl implements UserAccountService {
+
+    UserAccountRepository userAccountRepository;
+    @Autowired
+    public UserAccountServiceImpl(UserAccountRepository userAccountRepository){
+        this.userAccountRepository = userAccountRepository;
+    }
+
+    @Override
+    public UserAccount login(LoginUserData user) throws Exception {
+        UserAccount userAccount = userAccountRepository.findByAccount(user.getNumber());
+        if(userAccount==null){
+            throw new Exception("账号不存在");
+        }
+        if(userAccount.getPermission().equals("false")){
+            throw new Exception("账号目前无权限登录");
+        }
+        return userAccount;
+    }
+}

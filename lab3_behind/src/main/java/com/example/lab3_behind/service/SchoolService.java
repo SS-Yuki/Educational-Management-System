3c4f4dfd2be0d7560cc630296cd971a81b9accbd
YangTianqi <1211325315@qq.com>
2022-04-11 11:46:27 +0800

    增加了增加学院的service

--- lab3_behind/src/main/java/com/example/lab3_behind/service/SchoolService.java
+++ lab3_behind/src/main/java/com/example/lab3_behind/service/SchoolService.java
@@ -1,7 +1,9 @@
 package com.example.lab3_behind.service;
 
 import com.example.lab3_behind.domain.Major;
+import com.example.lab3_behind.domain.School;
 import com.example.lab3_behind.domain.dto.MajorAddingData;
+import com.example.lab3_behind.domain.dto.SchoolAddingData;
 import com.example.lab3_behind.domain.dto.SchoolAndMajorsData;
 
 import java.util.List;
@@ -9,4 +11,5 @@ import java.util.List;
 public interface SchoolService {
     List<SchoolAndMajorsData> getAllSchoolAndMajors();
     Major insertMajor(MajorAddingData majorData) throws Exception;
+    School insertSchool(SchoolAddingData schoolData) throws Exception;
 }

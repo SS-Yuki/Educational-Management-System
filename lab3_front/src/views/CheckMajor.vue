163e1a4204fdc1577fcb16dd38eb6c5dfad39ce4
wangjunfei <471612627@qq.com>
2022-04-12 09:30:34 +0800

    修major

--- lab3_front/src/views/CheckMajor.vue
+++ lab3_front/src/views/CheckMajor.vue
@@ -2,16 +2,18 @@
   <div class="checkmajor">
     <div>
       <div class="add" style="margin: 10px 0">
-        <el-button size="large" @click="input" type="primary">新增</el-button>
+        <el-button size="large" @click="add" type="primary">新增</el-button>
+        <el-input clearable v-model="search" placeholder="请输入关键字" style="width:50%;margin-left: 100px"></el-input>
+        <el-button type="primary" style="margin-left: 5px" @click="load">搜索</el-button>
       </div>
       <el-table :data="tableData" style="width: 100%" border stripe>
-        <el-table-column prop="major" label="专业" width="120" />
-        <el-table-column prop="school" label="学院" width="120" />
+        <el-table-column prop="majorName" label="专业" width="120" />
+        <el-table-column prop="schoolName" label="学院" width="120" />
         <el-table-column prop="introduction" label="简介" width="600" />
         <el-table-column fixed="right" label="操作" width="120">
-          <template #default>
-            <el-button type="text" size="small" @click="handleClick">编辑</el-button>
-            <el-popconfirm title="确认删除?">
+          <template #default="scope">
+            <el-button type="text" size="small" @click="handleEdit(scope.row.majorName,scope.row.schoolName)">编辑</el-button>
+            <el-popconfirm title="确认删除?" @confirm="handleDelete(scope.row.majorName,scope.row.schoolName)">
               <template #reference>
                 <el-button type="text">删除</el-button>
               </template>
@@ -36,19 +38,41 @@
     </div>
     <div>
       <el-dialog v-model="dialogVisible" title="添加新用户" width="30%">
-        <el-form :model="form" label-width="120px">
-          <el-form-item label="新院系">
-            <el-input v-model="form.new_school" />
-          </el-form-item>
+        <el-form :model="addSchool" label-width="120px">
           <el-form-item label="新专业">
-            <el-input v-model="form.new_major" />
+            <el-input v-model="addMajor.majorName" />
+          </el-form-item>
+          <el-form-item label="院系">
+            <el-input v-model="addMajor.schoolName" />
+<!--            <el-cascader  v-model="addMajor.schoolName" :options="options" @change="school_major_select"/>-->
           </el-form-item>
           <el-form-item label="新介绍">
-            <el-input v-model="form.new_introduction" />
+            <el-input v-model="addMajor.introduction"/>
           </el-form-item>
           <span class="dialog-footer">
         <el-button @click="dialogVisible = false">取消</el-button>
-        <el-button type="primary" @click="send_newpeople">确认</el-button>
+        <el-button type="primary" @click="save">确认</el-button>
+      </span>
+        </el-form>
+        <template #footer>
+        </template>
+      </el-dialog>
+    </div>
+    <div>
+      <el-dialog v-model="dialogVisible2" title="编辑信息" width="30%">
+        <el-form :model="newSchool" label-width="120px">
+          <el-form-item label="新院系">
+            <el-input v-model="majorNewName" />
+          </el-form-item>
+          <el-form-item label="院系">
+            <el-input v-model="majorNewSchool" />
+          </el-form-item>
+          <el-form-item label="新介绍">
+            <el-input v-model="introduction"/>
+          </el-form-item>
+          <span class="dialog-footer">
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="saveEdit">确认</el-button>
       </span>
         </el-form>
         <template #footer>
@@ -65,43 +89,97 @@ export default {
   name: "CheckMajor",
   data(){
     return{
+      options:[],
       total:0,
       pageSize:10,
       currentPage:1,
       search:'',
       dialogVisible:false,
-      new_major:{
-        new_major:'',
-        new_school:'',
-        new_introduction:''
+      dialogVisible2:false,
+      majorOldName:'',
+      majorNewName:'',
+      introduction:'',
+      majorOldSchool:'',
+      majorNewSchool:'',
+      addMajor:{
+        majorName:'',
+        schoolName:'',
+        introduction:''
       },
-      form:{},
+      // newSchool:{
+      //   oldName:'',
+      //   newName:'',
+      //   introduction:''
+      // },
       tableData:[]
     }
   },
-  created() {
+  mounted() {
     this.load()
   },
   methods:{
+    school_major_select(){
+
+    },
     load(){
-      request.get("/api/checkinfo",{
-        pageNum:this.currentPage,
-        pageSize:this.pageSize, //每页的条目数
-        search:this.search
-      }).then(res=>{
+      console.log(this.pageData)
+      request.post("/admin/findMajorPage",{
+            pageNum: this.currentPage,
+            pageSize: this.pageSize,
+            search: this.search
+          }
+      ).then(res=>{
         console.log(res)
-        this.tableData=res.data.records
-        this.total=res.data.total
+        if(res.data.code===200){
+          this.tableData=res.data.data.records
+          this.total=res.data.data.total
+        }
+        else{
+          this.$message({
+            type:"fail",
+            message: "失败"
+          })
+        }
       })
     },
-    input:function (){
+    add:function (){
       this.dialogVisible=true
-      this.new_major={}
+      this.addMajor={}
     },
-    send_newpeople:function (){
-      this.dialogVisible=false
-      request.post("/user/checkinfo",this.new_major).then(res=>{
+    save:function (){
+      request.post("/admin/addMajor", this.addMajor).then(res => {
         console.log(res)
+        this.load() // 刷新表格的数据
+        this.dialogVisible = false  // 关闭弹窗
+      })
+    },
+    saveEdit(){
+      console.log(this.newSchool)
+      request.post("/admin/updateMajorInfo", {
+        majorOldName:this.majorOldName,
+        majorNewName:this.majorNewName,
+        introduction:this.introduction,
+        majorOldSchool:this.majorOldSchool,
+        majorNewSchool:this.majorNewSchool
+      }).then(res=>{
+        console.log(res)
+        this.load()
+        this.dialogVisible2=false
+      })
+    },
+    handleEdit(majorName,schoolName){
+      this.majorOldName=majorName
+      this.majorOldSchool=schoolName
+      this.majorNewSchool=schoolName
+      this.dialogVisible2 = true
+      this.newMajor={}
+    },
+    handleDelete(majorName,schoolName) {
+      request.post("/admin/deleteMajor", {
+        majorName:majorName,
+        schoolName:schoolName
+      }).then(res => {
+        this.load()  // 删除之后重新加载表格的数据
       })
     },
     handleClick:function (){
@@ -110,8 +188,9 @@ export default {
     handleSizeChange:function (){
 
     },
-    handleCurrentChange:function (){
-
+    handleCurrentChange:function (pageNum){
+      this.currentPage = pageNum
+      this.load()
     }
   }
 }

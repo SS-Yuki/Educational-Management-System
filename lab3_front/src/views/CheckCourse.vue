3b895ff18ddad5c9ac57f2b78054cbcfb436bc8d
wangjunfei <471612627@qq.com>
2022-04-12 18:01:25 +0800

    修course

--- lab3_front/src/views/CheckCourse.vue
+++ lab3_front/src/views/CheckCourse.vue
@@ -257,6 +257,7 @@ export default {
     },
     handleEdit(id,courseName,courseNumber,teacherNum,major,school,classPeriod,classroom,
     creditHours,credits,capacity,introduction,applicant){
+      this.dialogVisible=true
       this.editCourse.id=id
       this.editCourse.courseName=courseName
       this.editCourse.courseNumber=courseNumber

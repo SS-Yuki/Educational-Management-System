6bf031492be37c269b1fbc40ee3ae624dc0d1ee9
YuKang <2833721527@qq.com>
2022-04-12 18:47:36 +0800

    用户信息组件添加

---
+++ lab3_front/src/components/UserInfo.vue
@@ -0,0 +1,112 @@
+<template>
+  <div>
+    <div class="user">
+      <el-dropdown>
+        <span class="el-dropdown-link">
+          你好, {{user_num}}
+          <el-icon class="el-icon--right">
+            <arrow-down/>
+          </el-icon>
+        </span>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item @click="pop">修改密码</el-dropdown-item>
+            <el-dropdown-item @click="this.$router.push('/teacher')">返回首页</el-dropdown-item>
+            <el-dropdown-item divided @click="logout">登出</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
+
+    <div>
+      <el-dialog v-model="dialogVisible" title="Tips" width="30%">
+        <el-form :model="change_pass_set" label-width="120px">
+          <el-form-item label="旧密码">
+            <el-input v-model="change_pass_set.old_pass" />
+          </el-form-item>
+          <el-form-item label="新密码">
+            <el-input v-model="change_pass_set.new_pass" />
+          </el-form-item>
+        </el-form>
+        <span class="dialog-footer">
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="change_pass">确认</el-button>
+      </span>
+        <template #footer>
+        </template>
+      </el-dialog>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import request from "@/utils/request";
+import {createRouter, createWebHistory} from "vue-router";
+import router, {routes} from "@/router";
+export default {
+  name: "UserInfo",
+  data() {
+    return {
+      user_num: '',
+      dialogVisible:false,
+      change_pass_set: {
+        old_pass: '',
+        new_pass: ''
+      }
+    }
+  },
+  components:{
+  },
+  mounted() {
+    this.user_num = JSON.parse(sessionStorage.getItem("user")).number
+  },
+  methods: {
+    logout: function () {
+      request.post("/user/logout", JSON.parse(sessionStorage["user"]).token).then(res => {
+        console.log(res)
+        console.log(res.data.code)
+        if (res.data.code === 200) {
+          this.$message({
+            type: "success",
+            message: res.data.msg
+          })
+        }
+        router = createRouter({
+          history: createWebHistory(process.env.BASE_URL),
+          routes
+        })
+        sessionStorage["routes"] = JSON.stringify(router.getRoutes())
+        sessionStorage.removeItem("user")
+        this.$router.push("/login")
+      })
+    },
+    pop: function () {
+      this.dialogVisible=true
+    },
+    change_pass: function () {
+      request.post("/user/changePassword", this.change_pass_set).then(res => {
+        console.log(res)
+        if (res.data.code === 200) {
+          this.$message({
+            type: "success",
+            message: res.data.msg
+          })
+        }
+        else {
+          this.$message({
+            type: "error",
+            message: res.data.msg
+          })
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.user {
+  float: right;
+}
+</style>
--- lab3_front/src/views/Student.vue
+++ lab3_front/src/views/Student.vue
@@ -2,45 +2,7 @@
   <div class="header">
     复旦大学学生系统
   </div>
-
-  <div class="user">
-    <el-dropdown>
-        <span class="el-dropdown-link">
-          你好, {{user_num}}
-          <el-icon class="el-icon--right">
-            <arrow-down/>
-          </el-icon>
-        </span>
-      <template #dropdown>
-        <el-dropdown-menu>
-          <el-dropdown-item @click="pop">修改密码</el-dropdown-item>
-          <el-dropdown-item @click="this.$router.push('/student')">返回首页</el-dropdown-item>
-          <el-dropdown-item divided @click="logout">登出</el-dropdown-item>
-        </el-dropdown-menu>
-      </template>
-    </el-dropdown>
-  </div>
-
-  <div>
-    <el-dialog v-model="dialogVisible" title="Tips" width="30%">
-      <el-form :model="change_pass_set" label-width="120px">
-        <el-form-item label="旧密码">
-          <el-input v-model="change_pass_set.old_pass" />
-        </el-form-item>
-        <el-form-item label="新密码">
-          <el-input v-model="change_pass_set.new_pass" />
-        </el-form-item>
-      </el-form>
-      <span class="dialog-footer">
-        <el-button @click="dialogVisible = false">取消</el-button>
-        <el-button type="primary" @click="change_pass">确认</el-button>
-      </span>
-      <template #footer>
-      </template>
-    </el-dialog>
-  </div>
-
-
+  <UserInfo/>
   <el-divider />
   <div class="student_menu">
     <el-row class="tac" style="width: 400px">
@@ -76,71 +38,13 @@
 </template>
 
 <script>
-import request from "@/utils/request";
-import {createRouter, createWebHistory} from "vue-router";
-import router, {routes} from "@/router";
-
+import UserInfo from "@/components/UserInfo";
 export default {
   name: "Student",
-  data() {
-    return {
-      user_num: '',
-      dialogVisible:false,
-      change_pass_set: {
-        old_pass: '',
-        new_pass: ''
-      }
-    }
-  },
-  mounted() {
-    this.user_num = JSON.parse(sessionStorage.getItem("user")).number
-  },
+
   components:{
-
+    UserInfo
   },
-
-  methods: {
-    logout: function () {
-      request.post("/user/logout", JSON.parse(sessionStorage["user"]).token).then(res => {
-        console.log(res)
-        console.log(res.data.code)
-        if (res.data.code === 200) {
-          this.$message({
-            type: "success",
-            message: res.data.msg
-          })
-        }
-        router = createRouter({
-          history: createWebHistory(process.env.BASE_URL),
-          routes
-        })
-        sessionStorage["routes"] = JSON.stringify(router.getRoutes())
-        sessionStorage.removeItem("user")
-        this.$router.push("/login")
-      })
-    },
-    pop: function () {
-      this.dialogVisible=true
-    },
-    change_pass: function () {
-      request.post("/user/changePassword", this.change_pass_set).then(res => {
-        console.log(res)
-        if (res.data.code === 200) {
-          this.$message({
-            type: "success",
-            message: res.data.msg
-          })
-        }
-        else {
-          this.$message({
-            type: "error",
-            message: res.data.msg
-          })
-        }
-      })
-    }
-  }
-
 
 
 }
@@ -162,7 +66,5 @@ export default {
   text-shadow : rgba(255,255,255,0.5) 0 5px 6px, rgba(255,255,255,0.2) 1px 3px 3px;
   -webkit-background-clip : text;
 }
-.user {
-  float: right;
-}
+
 </style>

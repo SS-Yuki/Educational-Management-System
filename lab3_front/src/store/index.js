03b6c50761bc5c9680ac8eb95ea408c68ab82d42
YuKang <2833721527@qq.com>
2022-04-15 15:00:18 +0800

    清除无用变量

--- lab3_front/src/store/index.js
+++ lab3_front/src/store/index.js
@@ -2,14 +2,10 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    routes_store: ''
   },
   getters: {
   },
   mutations: {
-    change_routes(state, now_routes) {
-      state.routes_store = now_routes
-    }
   },
   actions: {
   },

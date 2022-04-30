3468405dac9d806f7db38855325941ec9cf5acdc
YuKang <2833721527@qq.com>
2022-04-30 16:27:38 +0800

    页面布局

--- lab3_front/src/views/Admin.vue
+++ lab3_front/src/views/Admin.vue
@@ -1,94 +1,96 @@
 <template>
-  <div class="header">
-    复旦大学管理员系统
+  <div class="common-layout">
+    <el-container>
+      <el-header class="header">
+        复旦大学管理员系统
+        <UserInfo/>
+        <el-divider/>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">
+          <el-row class="tac" style="width: 400px">
+            <el-col :span="12">
+              <el-menu default-active="2" class="el-menu-vertical-demo">
+                <el-sub-menu index="1">
+                  <template #title>
+                    <!--              <el-icon><location /></el-icon>-->
+                    <span>用户信息管理</span>
+                  </template>
+                  <el-menu-item-group>
+                    <el-menu-item index="1-1">
+                      <router-link to="/admin/checkstudentinfo">查看学生信息</router-link>
+                    </el-menu-item>
+                    <el-menu-item index="1-2">
+                      <router-link to="/admin/checkteacherinfo">查看老师信息</router-link>
+                    </el-menu-item>
+                  </el-menu-item-group>
+                </el-sub-menu>
+                <el-sub-menu index="2">
+                  <template #title>
+                    <!--              <el-icon><location /></el-icon>-->
+                    <span>院系信息管理</span>
+                  </template>
+                  <el-menu-item-group>
+                    <el-menu-item index="2-1">
+                      <router-link to="/admin/checkmajor">查看专业</router-link>
+                    </el-menu-item>
+                  </el-menu-item-group>
+                  <el-menu-item-group>
+                    <el-menu-item index="2-2">
+                      <router-link to="/admin/checkschool">查看院系</router-link>
+                    </el-menu-item>
+                  </el-menu-item-group>
+                </el-sub-menu>
+                <el-sub-menu index="3">
+                  <template #title>
+                    <!--              <el-icon><location /></el-icon>-->
+                    <span>教务信息管理</span>
+                  </template>
+                  <el-menu-item-group>
+                    <el-menu-item index="3-1">
+                      <router-link to="/admin/checkbuilding">教学楼管理</router-link>
+                    </el-menu-item>
+                    <el-menu-item index="3-2">
+                      <router-link to="/admin/checkclassroom">教室管理</router-link>
+                    </el-menu-item>
+                    <el-menu-item index="3-3">
+                      <router-link to="/admin/checktime">上课时间管理</router-link>
+                    </el-menu-item>
+                  </el-menu-item-group>
+                </el-sub-menu>
+
+                <el-sub-menu index="4">
+                  <template #title>
+                    <!--              <el-icon><location /></el-icon>-->
+                    <span>课程信息管理</span>
+                  </template>
+                  <el-menu-item-group>
+                    <el-menu-item index="4-1">
+                      <router-link to="/admin/checkcourse">课程管理</router-link>
+                    </el-menu-item>
+                    <el-menu-item index="4-2">
+                      <router-link to="/admin/checkapplication">教师申请管理</router-link>
+                    </el-menu-item>
+                    <el-menu-item index="4-3">
+                      <router-link to="/admin/checkcourseopen">选课开放管理</router-link>
+                    </el-menu-item>
+                  </el-menu-item-group>
+                </el-sub-menu>
+
+              </el-menu>
+            </el-col>
+          </el-row>
+        </el-aside>
+        <el-container style="padding-top: 50px">
+          <el-main>
+            <router-view></router-view>
+          </el-main>
+        </el-container>
+      </el-container>
+    </el-container>
   </div>
-  <UserInfo/>
-  <el-divider />
-  <div class="admin_menu">
-    <el-row class="tac" style="width: 400px">
-      <el-col :span="12">
-        <el-menu
-            default-active="2"
-            class="el-menu-vertical-demo"
-            @open="handleOpen"
-            @close="handleClose"
-        >
-          <el-sub-menu index="1">
-            <template #title>
-              <el-icon><location /></el-icon>
-              <span>用户信息管理</span>
-            </template>
-            <el-menu-item-group>
-              <el-menu-item index="1-1">
-                <router-link to="/admin/checkstudentinfo">查看学生信息</router-link>
-              </el-menu-item>
-              <el-menu-item index="1-2">
-                <router-link to="/admin/checkteacherinfo">查看老师信息</router-link>
-              </el-menu-item>
-            </el-menu-item-group>
-          </el-sub-menu>
-          <el-sub-menu index="1">
-            <template #title>
-              <el-icon><location /></el-icon>
-              <span>院系信息管理</span>
-            </template>
-            <el-menu-item-group>
-              <el-menu-item index="1-1">
-                <router-link to="/admin/checkmajor">查看专业</router-link>
-              </el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group>
-              <el-menu-item index="1-2">
-                <router-link to="/admin/checkschool">查看院系</router-link>
-              </el-menu-item>
-            </el-menu-item-group>
-          </el-sub-menu>
-          <el-sub-menu index="1">
-            <template #title>
-              <el-icon><location /></el-icon>
-              <span>教务信息管理</span>
-            </template>
-            <el-menu-item-group>
-              <el-menu-item index="1-1">
-                <router-link to="/admin/checkbuilding">教学楼管理</router-link>
-              </el-menu-item>
-              <el-menu-item index="1-2">
-                <router-link to="/admin/checkclassroom">教室管理</router-link>
-              </el-menu-item>
-              <el-menu-item index="1-3">
-                <router-link to="/admin/checktime">上课时间管理</router-link>
-              </el-menu-item>
-            </el-menu-item-group>
-          </el-sub-menu>
-
-          <el-sub-menu index="1">
-            <template #title>
-              <el-icon><location /></el-icon>
-              <span>课程信息管理</span>
-            </template>
-            <el-menu-item-group>
-              <el-menu-item index="1-1">
-                <router-link to="/admin/checkcourse">课程管理</router-link>
-              </el-menu-item>
-              <el-menu-item index="1-2">
-                <router-link to="/admin/checkapplication">教师申请管理</router-link>
-              </el-menu-item>
-              <el-menu-item index="1-3">
-                <router-link to="/admin/checkcourseopen">选课开放管理</router-link>
-              </el-menu-item>
-            </el-menu-item-group>
-          </el-sub-menu>
-
-        </el-menu>
-      </el-col>
-    </el-row>
-  </div>
-
-  <div>
-    <router-view></router-view>
-  </div>
-
 </template>
+
 
 <script>
 
@@ -105,13 +107,6 @@ export default {
 </script>
 
 <style scoped>
-.admin_menu{
-  width:400px;
-  display: flex;
-  position:relative;
-  box-sizing: border-box;
-  float: left;
-}
 .header{
   height:60px;
   font-size: 50px;
@@ -119,13 +114,6 @@ export default {
   background-color : black;
   text-shadow : rgba(255,255,255,0.5) 0 5px 6px, rgba(255,255,255,0.2) 1px 3px 3px;
   -webkit-background-clip : text;
-}
-a {
-  text-decoration: none;
-}
-
-.router-link-active {
-  text-decoration: none;
 }
 
 </style>

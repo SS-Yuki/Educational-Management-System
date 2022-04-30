86a484422fab5bbd61ace2ff773a634b89c0b13e
zhangjiaxun <20302010032@fudan.edu.cn>
2022-04-30 14:59:47 +0800

    删除多余的类，为Year和Semester添加Lombok注解

--- lab3_behind/src/main/java/com/example/lab3_behind/domain/dto/YearAndSemestersData.java
+++ lab3_behind/src/main/java/com/example/lab3_behind/domain/dto/YearAndSemestersData.java
@@ -1,7 +1,14 @@
 package com.example.lab3_behind.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class YearAndSemestersData {
     String year;
     List<String> semesters;

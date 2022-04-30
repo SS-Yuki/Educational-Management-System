9fa803094195d8de17ec0a49aa86ae60aa29ed11
zhangjiaxun <20302010032@fudan.edu.cn>
2022-04-30 19:43:51 +0800

    allSemesters和allTime两个端口

--- lab3_behind/src/main/java/com/example/lab3_behind/controller/CommonVisitComtroller.java
+++ lab3_behind/src/main/java/com/example/lab3_behind/controller/CommonVisitComtroller.java
@@ -1,7 +1,13 @@
 package com.example.lab3_behind.controller;
 
+import com.example.lab3_behind.common.TimeData;
+import com.example.lab3_behind.domain.TimeTable;
 import com.example.lab3_behind.domain.dto.YearAndSemestersData;
+import com.example.lab3_behind.domain.dto.YearSemesterPair;
 import com.example.lab3_behind.domain.resp.Result;
+import com.example.lab3_behind.service.TeachingAffairsService;
+import com.example.lab3_behind.utils.TimeTool;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,19 +20,24 @@ import java.util.Map;
 @RequestMapping("common")
 public class CommonVisitComtroller {
 
+    @Autowired
+    TeachingAffairsService teachingAffairsService;
     @RequestMapping("allSemesters")
     public Result allSemesters(){
         Map<String,Object> map = new HashMap<>();
-        map.put("defaultYear","2021-2022");
-        map.put("defaultSemester","春季学期");
-        List<String> semesters = new ArrayList<>();
-        semesters.add("春季学期");
-        semesters.add("秋季学期");
-        YearAndSemestersData yearAndSemestersData = new YearAndSemestersData("2021-2022",semesters);
-        List<YearAndSemestersData> yearAndSemestersDatas = new ArrayList<>();
-        yearAndSemestersDatas.add(yearAndSemestersData);
-        map.put("yearAndSemesters",yearAndSemestersDatas);
+        YearSemesterPair yearSemesterPair = TimeTool.getPresentYearAndSemester();
+        map.put("defaultYear",yearSemesterPair.getYear());
+        map.put("defaultSemester",yearSemesterPair.getSemester());
+        map.put("yearAndSemesters",teachingAffairsService.getAllYearAndSemesters());
+        return Result.succ(map);
+    }
 
+    @RequestMapping("/allTime")
+    public Result allTime(){
+        Map<String,Object> map = new HashMap<>();
+        List<TimeTable> timeTables = teachingAffairsService.findAllTimeTable();
+        List<TimeData> timeDatas = TimeData.getContents(timeTables);
+        map.put("times",timeDatas);
         return Result.succ(map);
     }
 }

f8dbc1c3fb3aad1a8fd324c28ac1681e63e8bda1
zhangjiaxun <20302010032@fudan.edu.cn>
2022-05-03 15:17:13 +0800

    完善pageTool

--- lab3_behind/src/main/java/com/example/lab3_behind/utils/MyPageTool.java
+++ lab3_behind/src/main/java/com/example/lab3_behind/utils/MyPageTool.java
@@ -6,8 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MyPageTool {
-    public static MyPage<Object> getPage(List<Object> allData,Integer size,Integer pageNum){
-        MyPage<Object> ans = new MyPage<>();
+    public static <T> MyPage<T> getPage(List<T> allData,Integer size,Integer pageNum){
+        MyPage<T> ans = new MyPage<>();
         ans.setTotal(allData.size());
         ans.setRecords(new ArrayList<>());
         for(int i=(pageNum-1)*size; i<pageNum*size && i<ans.getTotal() && i>=0;i++){

76f8524de24fcb1593161ab0e7b7d25a29d91f90
YangTianqi <1211325315@qq.com>
2022-05-04 12:01:37 +0800

    修复了bug

--- lab3_behind/src/main/java/com/example/lab3_behind/repository/SelectCourseApplicationRepository.java
+++ lab3_behind/src/main/java/com/example/lab3_behind/repository/SelectCourseApplicationRepository.java
@@ -1,9 +1,10 @@
 package com.example.lab3_behind.repository;
 
+import com.example.lab3_behind.domain.SelectCourseApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SelectCourseApplicationRepository extends JpaRepository<SelectCourseApplicationRepository, Long> {
+public interface SelectCourseApplicationRepository extends JpaRepository<SelectCourseApplication, Long> {
 
 }

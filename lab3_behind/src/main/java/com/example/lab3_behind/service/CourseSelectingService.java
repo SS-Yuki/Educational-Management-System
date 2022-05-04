701349e41a886d81c66b1172dd5421d9258cd576
YangTianqi <1211325315@qq.com>
2022-05-04 19:51:55 +0800

    增加了打选课申请的相关功能

--- lab3_behind/src/main/java/com/example/lab3_behind/service/CourseSelectingService.java
+++ lab3_behind/src/main/java/com/example/lab3_behind/service/CourseSelectingService.java
@@ -10,9 +10,9 @@ import java.util.List;
 public interface CourseSelectingService {
     void selectCourse(String sutNum, Integer courseId, SchoolYear openYear, Semester openSemester) throws Exception;
     void dropCourse(String sutNum, Integer courseId, SchoolYear openYear, Semester openSemester) throws Exception;
-    void pushCourseSelectingApplication(String stuNum, Integer courseId);
-    void approveSelectCourseApplication(Integer applicationId, String commends);
-    void rejectSelectCourseApplication(Integer applicationId, String commends);
-    MyPage<SelectCourseApplication> findAPageSelectCourseApplicationToDeal();
+    void pushCourseSelectingApplication(String stuNum, Integer courseId, String reason) throws Exception;
+    void approveSelectCourseApplication(Integer applicationId, String commends) throws Exception;
+    void rejectSelectCourseApplication(Integer applicationId, String commends) throws Exception;
+    MyPage<SelectCourseApplication> findAPageSelectCourseApplicationToDeal(Integer page, Integer size);
     List<SelectCourseApplication> findMySelectCourseApplication(String stuNum);
 }
